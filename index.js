bc7195f9f3aaf71555b58b9fb536d7ddbd311c8b
joshu <67325566+chanjoshu@users.noreply.github.com>
2020-07-11 12:06:08 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -2,13 +2,16 @@
 const result = document.getElementById("gyudon");
 const btn = document.getElementById("btn");
 btn.addEventListener("click", () => {
-    const i = Math.floor(Math.random() * 3);
+    const i = Math.floor(Math.random() * 4);
 
     if (i == 0) {
         result.textContent = "松屋";
     } else if (i == 1) {
         result.textContent = "吉野家";
-    } else {
+    } else if (i == 2) {
         result.textContent = "すきや";
+    }
+    else {
+        result.textContent = "なか卯";
     }
 })
